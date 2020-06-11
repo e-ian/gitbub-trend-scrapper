1843c604891cf578a05c5d9621b3acbb0d42150c
e-ian <ianemma70@gmail.com>
2020-06-11 16:25:40 +0300

    add for loop to extract details

--- github-trending.py
+++ github-trending.py
@@ -8,9 +8,23 @@ page = requests.get('https://github.com/trending')
 soup = BeautifulSoup(page.text, 'html.parser')
 
 # get repo list
-# repo = soup.find(class_="Box")
+repo = soup.find(class_="explore-pjax-container container-lg p-responsive pt-6")
 
 # find all instances of that class (return 25 as shown on github main page)
-repo_list = soup.find_all(class_="Box-row")
+repo_list = repo.find_all(class_="Box-row")
 
 print(len(repo_list))
+
+for repo in repo_list:
+    # find first <h1> tag and get the text. split text with '/' to get an array with developer name and repo name
+    full_repo_name = repo.find('h1').text.split('/')
+    # extract the developer name at index 0
+    developer = full_repo_name[0].strip()
+    # extract the repo name at index 1
+    repo_name = full_repo_name[1].strip()
+    # first occurence of class octicon octicon-star and get the text from the parent(number of stars)
+    stars = repo.find(class_="octicon octicon-star").parent.text.strip()
+    
+    print('developer', developer)
+    print('name', repo_name)
+    print('stars', stars)
