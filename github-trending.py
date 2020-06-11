7a7d555506e8f41fb1fa6c8bc2944fb508fb0b28
e-ian <ianemma70@gmail.com>
2020-06-11 14:42:55 +0300

    data extraction

--- github-trending.py
+++ github-trending.py
@@ -3,9 +3,14 @@ from bs4 import BeautifulSoup
 
 # Collect the github page
 page = requests.get('https://github.com/trending')
-print(page)
 
 #create a BeautifulSoup object
 soup = BeautifulSoup(page.text, 'html.parser')
 
-print(soup)
+# get repo list
+# repo = soup.find(class_="Box")
+
+# find all instances of that class (return 25 as shown on github main page)
+repo_list = soup.find_all(class_="Box-row")
+
+print(len(repo_list))
